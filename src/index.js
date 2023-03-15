358e2d7bcd7c8b2a7263e41c3fdc0b6effae70c7
ignatius chandra <ignatius.c.k@gmail.com>
2023-03-15 20:59:30 +0700

    add oauthGoogle, Update CRUD

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 const path = require("path");
 
 const express = require("express");
+const session = require("express-session");
 const cors = require("cors");
+const passport = require("passport");
 
 const fileConfig = require("./config/storage");
 const routes = require("./routes/index");
@@ -9,9 +11,27 @@ const routes = require("./routes/index");
 const app = express();
 
 app.use(express.json());
+app.use(
+  session({
+    resave: false,
+    saveUninitialized: true,
+    secret: "randomsecret",
+  })
+);
 app.use(cors());
 app.use(fileConfig);
 app.use("/images", express.static(path.join(__dirname, "images")));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.serializeUser((user, cb) => {
+  cb(null, user);
+});
+
+passport.deserializeUser((obj, cb) => {
+  cb(null, obj);
+});
 
 app.use(routes);
 
